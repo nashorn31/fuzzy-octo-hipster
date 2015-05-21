ea485b581df89a8fa8de653eb8b575289fe93797
Johannes <Johannes@Laptop-Johannes.Speedport_W_724V_09011603_00_009>
2015-05-21 09:17:50 +0200

    Javadoc

--- KarteDesRumtreibersWebserver/src/main/java/ticketSystem/TicketGuiBean.java
+++ KarteDesRumtreibersWebserver/src/main/java/ticketSystem/TicketGuiBean.java
@@ -16,6 +16,12 @@ import javax.persistence.EntityManager;
 
 import databaseconnection.InitEntityManager;
 
+/**
+ * Bean to display the tickets
+ * 
+ * @author Johannes
+ *
+ */
 @ManagedBean(name = "ticketBasicView")
 @ViewScoped
 public class TicketGuiBean implements Serializable {
@@ -31,6 +37,102 @@ public class TicketGuiBean implements Serializable {
 	private Date startDate;
 	private Date endDate;
 
+	/**
+	 * class to load deficiencies provided by the tomcat
+	 */
+	@ManagedProperty("#{ticketService}")
+	private ValuesForTicket service;
+
+	public ValuesForTicket getService() {
+		return service;
+	}
+
+	public void setService(ValuesForTicket service) {
+		this.service = service;
+	}
+
+	/**
+	 * executed after the bean is created to load the deficiencies
+	 */
+	@PostConstruct
+	public void init() {
+		setDeficiencies(service.getAllTickets(null, null, null, null, null));
+	}
+
+	/**
+	 * reload the deficiencies after a filter is set
+	 */
+	public void onFilterChangedStatus() {
+		setDeficiencies(service.getAllTickets(statusFilter, ticketID,
+				roomNumber, startDate, endDate));
+	}
+
+	/**
+	 * change the status from a specific deficiency
+	 * 
+	 * @param deficiency
+	 *            deficiency on which the status should be changed
+	 * @param status
+	 *            status to which the should be changed
+	 */
+	public void changeStatus(Deficiencies deficiency, String status) {
+		deficiency.setStatus(status);
+
+	}
+
+	/**
+	 * Merge a specific deficiency to the database
+	 * 
+	 * @param deficiency
+	 *            Deficiency which should be merged to the database
+	 */
+	public void changeStatus(Deficiencies deficiency) {
+		EntityManager em = InitEntityManager.getEntityManager();
+		em.getTransaction().begin();
+		em.merge(deficiency);
+		em.getTransaction().commit();
+
+	}
+
+	/**
+	 * Merge all changed statuses
+	 */
+	public void changeAllStatus() {
+		EntityManager em = InitEntityManager.getEntityManager();
+
+		for (Deficiencies d : deficiencies) {
+			em.getTransaction().begin();
+			em.merge(d);
+			em.getTransaction().commit();
+		}
+
+	}
+
+	/**
+	 * list of all statuses needed to the display the deficiencies
+	 */
+	public Map<String, String> getStati() {
+		Map<String, String> stati = new TreeMap<String, String>();
+		stati.put("Offen", "OPEN");
+		stati.put("In Arbeit", "IN WORK");
+		stati.put("Geschlossen", "CLOSED");
+		return stati;
+	}
+
+	/**
+	 * list of all statuses needed for the filter
+	 */
+	public Map<String, String> getStatiAll() {
+		Map<String, String> stati = new TreeMap<String, String>();
+		stati.put("Alle", "ALL");
+		stati.put("Offen", "OPEN");
+		stati.put("In Arbeit", "IN WORK");
+		stati.put("Geschlossen", "CLOSED");
+
+		return stati;
+	}
+
+	// just the getter and setter to access the variables
 	public String getTicketID() {
 		return ticketID;
 	}
@@ -45,85 +147,6 @@ public class TicketGuiBean implements Serializable {
 
 	public void setRoomNumber(String roomNumber) {
 		this.roomNumber = roomNumber;
-	}
-
-	public void onFilterChangedStatus() {
-
-		setDeficiencies(service.getAllTickets(statusFilter, ticketID,
-				roomNumber, startDate, endDate));
-
-	}
-
-	@ManagedProperty("#{ticketService}")
-	private ValuesForTicket service;
-
-	public ValuesForTicket getService() {
-		return service;
-	}
-
-	public void setService(ValuesForTicket service) {
-		this.service = service;
-	}
-
-	@PostConstruct
-	public void init() {
-		setDeficiencies(service.getAllTickets(null, null, null, null, null));
-	}
-
-	public List<Deficiencies> getDeficiencies() {
-		return deficiencies;
-	}
-
-	public void setDeficiencies(List<Deficiencies> deficiencies) {
-		this.deficiencies = deficiencies;
-	}
-
-	public void changeStatus(Deficiencies deficiencies, String status) {
-		deficiencies.setStatus(status);
-
-	}
-
-	public void changeStatus(Deficiencies deficiencies) {
-		EntityManager em = InitEntityManager.getEntityManager();
-		em.getTransaction().begin();
-		em.merge(deficiencies);
-		em.getTransaction().commit();
-
-	}
-
-	public void changeAllStatus() {
-		EntityManager em = InitEntityManager.getEntityManager();
-
-		for (Deficiencies d : deficiencies) {
-			em.getTransaction().begin();
-			em.merge(d);
-			em.getTransaction().commit();
-		}
-
-	}
-
-	/**
-	 * Liste Stati für Anzeige des Status
-	 */
-	public Map<String, String> getStati() {
-		Map<String, String> stati = new TreeMap<String, String>();
-		stati.put("Offen", "OPEN");
-		stati.put("In Arbeit", "IN WORK");
-		stati.put("Geschlossen", "CLOSED");
-		return stati;
-	}
-
-	/**
-	 * Liste Stati für Filter
-	 */
-	public Map<String, String> getStatiAll() {
-		Map<String, String> stati = new TreeMap<String, String>();
-		stati.put("Alle", "ALL");
-		stati.put("Offen", "OPEN");
-		stati.put("In Arbeit", "IN WORK");
-		stati.put("Geschlossen", "CLOSED");
-
-		return stati;
 	}
 
 	public String getStatusFilter() {
@@ -149,6 +172,14 @@ public class TicketGuiBean implements Serializable {
 
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
+	}
+
+	public List<Deficiencies> getDeficiencies() {
+		return deficiencies;
+	}
+
+	public void setDeficiencies(List<Deficiencies> deficiencies) {
+		this.deficiencies = deficiencies;
 	}
 
 }
--- KarteDesRumtreibersWebserver/src/main/java/ticketSystem/ValuesForTicket.java
+++ KarteDesRumtreibersWebserver/src/main/java/ticketSystem/ValuesForTicket.java
@@ -14,10 +14,41 @@ import javax.persistence.TypedQuery;
 import room.RoomSearch;
 import databaseconnection.InitEntityManager;
 
+/**
+ * service to load deficiencies
+ * 
+ * @author Johannes
+ *
+ */
 @ManagedBean(name = "ticketService")
 @ApplicationScoped
 public class ValuesForTicket {
 
+	/**
+	 * load all tickets from the database (limited to 500 records)
+	 * 
+	 * @return all tickets
+	 */
+	public List<Deficiencies> getAllTickets() {
+
+		return getAllTickets(null, null, null, null, null);
+	}
+
+	/**
+	 * load all tickets with specific attributes
+	 * 
+	 * @param status
+	 *            ticket status
+	 * @param ticketID
+	 *            id from the ticket
+	 * @param roomNumber
+	 *            number from the room where the deficiencies is reported
+	 * @param startDate
+	 *            only tickets after this date are loaded
+	 * @param endDate
+	 *            only tickets before this date are loaded
+	 * @return tickets with the specific attributes
+	 */
 	public List<Deficiencies> getAllTickets(String status, String ticketID,
 			String roomNumber, Date startDate, Date endDate) {
 
